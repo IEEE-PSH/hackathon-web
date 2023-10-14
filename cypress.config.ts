02a2ba966315891d023db197feebd31716b4196d
John Nguyen <johnnguyen4501@gmail.com>
2023-10-14 14:39:55 -0400

    get link of url

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "cypress";
 
+import { plugins } from "cypress-social-logins";
+const googleSocialLogin = plugins.GoogleSocialLogin;
+
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
+  },
+  env: {
+    EMAIL_KEY: "922133c6-4549-4ec6-af28-ac951b0ef45f",
+    EMAIL_NAMESPACE: "q5m8r",
   },
 });
--- cypress/e2e/themeTest.cy.ts
+++ cypress/e2e/themeTest.cy.ts
@@ -1,10 +1,25 @@
+import { slowCypressDown } from "cypress-slow-down";
+slowCypressDown(false);
+
 describe("theme propagation spec", () => {
   it("check if page theme is 'dark' after sign-in", () => {
     cy.visit("");
     cy.get('[test-id="mode-toggle"]').click();
     cy.get('[test-id="dark-theme"]').click();
     cy.contains("Login").click();
-    cy.get('[test-id="google-sign-in"]').click();
-    //can't get past sign in to test theme
+    cy.get('[name="email"]').type("q5m8r.test@inbox.testmail.app{enter}");
+
+    let url = "";
+    cy.request({
+      url: `https://api.testmail.app/api/json?apikey=${
+        Cypress.env().EMAIL_KEY
+      }&namespace=${Cypress.env().EMAIL_NAMESPACE}&pretty=true`,
+      failOnStatusCode: false,
+    }).then((res) => {
+      let url = res.body.emails[0].text.match(/\[(.*?)\]/)[0].slice(1, -1); //get magic link url in json
+      console.log(url);
+      cy.wait(1300);
+      cy.visit(url);
+    });
   });
 });
